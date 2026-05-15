21743e102a82ba70101bee9578c03b7937026ac8
HusseinMohty <eng.husseinmohty@gmail.com>
2026-05-15 05:39:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -175,7 +175,7 @@ function App() {
         <div className="photoGrid">
           <div>Field Visit Photo</div>
           <div>Training Photo</div>
-            <img src="/photos/cro logo.PNG.jpg" alt="Rehabilitation" />
+            <img src="/photos/cro.jpg" alt="Rehabilitation" />
         </div>
       </section>
 
