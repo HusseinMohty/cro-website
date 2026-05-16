975473fa5c9fedd7c190d9b0a829f61a31bf441f
HusseinMohty <eng.husseinmohty@gmail.com>
2026-05-16 03:51:04 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -191,9 +191,9 @@ function App() {
 
         <div className="contactCard">
           <div><Mail /> contact@crorelief.org</div>
-          <div><Phone /> +961 71721971</div>
+          <div><Phone /> +961 03123456</div>
           <div><MapPin /> Lebanon –Saida, Beirut & Mount Lebanon, and accessible areas</div>
-          <a className="primaryBtn full" href="mailto:contact@cro-initiative.org">Send Email</a>
+          <a className="primaryBtn full" href="mailto:contact@crorelief.org">Send Email</a>
         </div>
       </section>
 
