3b1ea3e0f23123bb8a433ec414914ab96bfc4fb5
HusseinMohty <eng.husseinmohty@gmail.com>
2026-05-16 04:33:36 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-// import MainWebsite from './MainWebsite'
-import Maintenance from './Maintenance'
+ import MainWebsite from './MainWebsite'
+//import Maintenance from './Maintenance'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Maintenance />
+  <MainWebsite />
 )
