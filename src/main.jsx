40b71b5a40a4aa591c904dcb64808d491aff6be7
HusseinMohty <eng.husseinmohty@gmail.com>
2026-05-16 04:18:25 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles.css'
-
-//import MainWebsite from './MainWebsite'
+// import MainWebsite from './MainWebsite'
 import Maintenance from './Maintenance'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Maintenance />
-  </React.StrictMode>
+  <Maintenance />
 )
