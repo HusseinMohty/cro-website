037be93a0d8d40c80ddb2de228119bb9f320c0ad
HusseinMohty <eng.husseinmohty@gmail.com>
2026-05-16 04:05:28 +0300

    Create Maintenance.jsx

---
+++ Maintenance.jsx
@@ -0,0 +1,17 @@
+export default function Maintenance() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: '#0f172a',
+      color: 'white',
+      fontFamily: 'Arial'
+    }}>
+      <h1>Website Under Maintenance</h1>
+      <p>We will be back shortly.</p>
+    </div>
+  )
+}
